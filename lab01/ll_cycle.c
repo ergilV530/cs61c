cfda56f04c32b75756a10e0140c3f8128310b75f
ergilV530 <moonnighttrees@icloud.com>
2026-04-08 22:20:40 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) {
+        return 0;
+    }
+    
+    struct node *tortoise = head;
+    struct node *hare = head;
+    
+    while (hare != NULL && hare->next != NULL) {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+        
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
+    
     return 0;
 }
