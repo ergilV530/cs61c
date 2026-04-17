6df750df81890737c645d757192d82dd5938eb80
ergilV530 <moonnighttrees@icloud.com>
2026-04-17 18:40:36 +0800

    lab02

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -6,6 +6,7 @@
 unsigned get_bit(unsigned x,
                  unsigned n) {
     // YOUR CODE HERE
+    return (x >> n) & 1;
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
@@ -17,11 +18,19 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    if (v == 1) {
+        *x = *x | (1u << n);
+    } else {
+        *x = *x & ~(1u << n);
+    }
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    unsigned v = get_bit(* x, n);
+    v = v ^ 1;
+    set_bit(x, n, v);
 }
 
